be49a53e6aed3a0293bd332d4876421203720f46
Joey Chen <joeyc43@nycstudents.net>
2025-10-24 15:35:17 -0400

    appointment book update

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -1,23 +1,41 @@
 public class AppointmentBook {
     private boolean[][] schedule;
-    public static void main(String[] args) {         
-    }
+
     public AppointmentBook(boolean[][] schedule) {
         this.schedule = schedule;
     }
+
     private boolean isMinuteFree(int period, int minute) {
         return schedule[period - 1][minute];
     }
-    public int findFreeBlock(int period, int duration) {
-        return 0;
-    }
+
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
+        schedule = new boolean[8][60];
+        for(int i = 25; i < 30; i++) schedule[1][i] = true;
+        for(int i = 0; i < 15; i++) schedule[2][i] = true;
+        for(int i = 41; i < 60; i++) schedule[2][i] = true;
+        for(int i = 5; i < 30; i++) schedule[3][i] = true;
+        for(int i = 44; i < 60; i++) schedule[3][i] = true;
+        AppointmentBook b = new AppointmentBook(schedule);
         return false;
     }
-    public void printPeriod(int period)
-    {
-        for(int i = 0; i <schedule[period - 1].length; i++)
+
+    public void printPeriod(int period) {
+        for (int i = 0; i < schedule[period - 1].length; i++)
             System.out.println(i + " " + schedule[period - 1][i]);
     }
+
+    public int findFreeBlock(int period, int duration) {
+        int block = 0;
+        for (int i = 0; i < 60; i++) {
+            if (isMinuteFree(period, i)) {
+                block++;
+                if (block == duration)
+                    return i - duration + 1;
+            } else block = 0;
+        }
+        return -1;
+    }
+
 }
 
