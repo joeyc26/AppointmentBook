e6b7a1954504ff5f54e1883ea5e97139a3f44523
Joey Chen <joeyc43@nycstudents.net>
2025-10-24 15:14:07 -0400

    appointment book

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class AppointmentBook {
+    private boolean[][] schedule;
+    public static void main(String[] args) {         
+    }
+    public AppointmentBook(boolean[][] schedule) {
+        this.schedule = schedule;
+    }
+    private boolean isMinuteFree(int period, int minute) {
+        return schedule[period - 1][minute];
+    }
+    public int findFreeBlock(int period, int duration) {
+        return 0;
+    }
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
+        return false;
+    }
+    public void printPeriod(int period)
+    {
+        for(int i = 0; i <schedule[period - 1].length; i++)
+            System.out.println(i + " " + schedule[period - 1][i]);
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        boolean[][] schedule = new boolean[8][60];
+        AppointmentBook a = new AppointmentBook(schedule);
+        for(int i = 0; i <15; i++) schedule [1][i] = true;
+    }
 }
